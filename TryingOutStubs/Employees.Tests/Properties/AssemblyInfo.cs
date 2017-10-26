4e81334d3880bb559a5c60f6f0928b9f1bd7c50b
Jim Blizzard <jimblizzard@hotmail.com>
2017-10-26 19:53:46 -0400

    initial

---
+++ TryingOutStubs/Employees.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MySUT.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MySUT.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("85b679f1-7a4c-41c8-9e83-5f8806578b54")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TryingOutStubs/Employees/EmployeeManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace Employees
+{ 
+    public class EmployeeManager
+    { 
+        // an example of passing an interface to a method
+        public int GetEmployeeIdByName(string eName, IEmployeeManagerHelper empMgrHelper = null)
+        {
+            // if the helper is null, we need to create one
+            if (empMgrHelper == null)
+            {
+                 empMgrHelper= new EmployeeManagerHelper();
+            }
+
+            var empId = empMgrHelper.GetAnEmp(eName);
+
+            return empId;
+        }
+    }
+
+    public interface IEmployeeManagerHelper
+    {
+        int GetAnEmp(string n);
+    } 
+
+    // notice that this implementation isn't quite ready. . .
+    public class EmployeeManagerHelper : IEmployeeManagerHelper
+    {
+        public int GetAnEmp(string name)
+        {
+            throw new Exception("This is not ready yet"); 
+        }
+    }
+}
